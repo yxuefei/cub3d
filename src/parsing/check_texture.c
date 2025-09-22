787c875182231ad57ae3e8415c5cfc1fc81cdd8d
yxuefei <yxuefeiluvbunny@gmail.com>
2025-09-23 00:49:23 +0200

    feat:check texture path and rgb

---
+++ src/parsing/check_texture.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_texture.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xueyang <xueyang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 00:38:47 by xueyang           #+#    #+#             */
+/*   Updated: 2025/09/23 00:48:31 by xueyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub.h"
+
+static int	check_file_exists_and_xpm(char *path)
+{
+	int		fd;
+	int		len;
+
+	if (!path)
+		return (0);
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	close(fd);
+	len = (int)ft_strlen(path);
+	if (len < 4)
+		return (0);
+	if (ft_strncmp(path + len - 4, ".xpm", 4) != 0)
+		return (0);
+	return (1);
+}
+
+int	validate_textures(t_cub_data *d)
+{
+	if (!d || !d->no || !d->so || !d->we || !d->ea)
+		return (0);
+	if (!check_file_exists_and_xpm(d->no))
+		return (0);
+	if (!check_file_exists_and_xpm(d->so))
+		return (0);
+	if (!check_file_exists_and_xpm(d->we))
+		return (0);
+	if (!check_file_exists_and_xpm(d->ea))
+		return (0);
+	if (d->floor_color < 0 || d->floor_color > 0xFFFFFF)
+		return (0);
+	if (d->ceiling_color < 0 || d->ceiling_color > 0xFFFFFF)
+		return (0);
+	return (1);
+}
--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: xueyang <xueyang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 01:24:20 by xueyang           #+#    #+#             */
-/*   Updated: 2025/09/16 01:25:19 by xueyang          ###   ########.fr       */
+/*   Updated: 2025/09/23 00:45:57 by xueyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,93 @@ int	get_map_height(char **map)
 	while (map[count])
 		count++;
 	return (count);
+}
+
+static void	skip_spaces(const char *s, int *i)
+{
+	while (s[*i] == ' ' || s[*i] == '\t')
+		(*i)++;
+}
+
+static int	to_int(const char *s, int *i)
+{
+	int	val;
+
+	val = 0;
+	skip_spaces(s, i);
+	if (!(s[*i] >= '0' && s[*i] <= '9'))
+		return (-1);
+	while (s[*i] >= '0' && s[*i] <= '9')
+	{
+		val = val * 10 + (s[*i] - '0');
+		(*i)++;
+	}
+	skip_spaces(s, i);
+	return (val);
+}
+
+/* "220, 100 ,0" -> 0xDC6400 */
+int	parse_color_string(const char *s)
+{
+	int	i;
+	int	r;
+	int	g;
+	int	b;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	r = to_int(s, &i);
+	if (r < 0 || s[i++] != ',')
+		return (-1);
+	g = to_int(s, &i);
+	if (g < 0 || s[i++] != ',')
+		return (-1);
+	b = to_int(s, &i);
+	if (b < 0)
+		return (-1);
+	skip_spaces(s, &i);
+	if (s[i] != '\0')
+		return (-1);
+	if (r > 255 || g > 255 || b > 255)
+		return (-1);
+	return ((r << 16) | (g << 8) | b);
+}
+
+char	*parse_texture_path(const char *s)
+{
+	int		i;
+	int		start;
+	int		end;
+	int		fd;
+	char	*path;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	skip_spaces(s, &i);
+	start = i;
+	while (s[i] && s[i] != '\n')
+		i++;
+	end = i;
+	while (end > start && (s[end - 1] == ' ' || s[end - 1] == '\t'))
+		end--;
+	path = ft_substr(s, start, end - start);
+	if (!path)
+		return (NULL);
+	/* 必须是 .xpm 结尾 */
+	if ((int)ft_strlen(path) < 4 || ft_strncmp(path + strlen(path) - 4, ".xpm", 4) != 0)
+	{
+		free(path);
+		return (NULL);
+	}
+	/* 检查文件能否打开 */
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+	{
+		free(path);
+		return (NULL);
+	}
+	close(fd);
+	return (path);
 }
