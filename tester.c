7c5c6ea2fcc306e988e6c184b3a1336f4ec6cfe6
Oleksii Malovichko <omalovic@3-A-9.42heilbronn.de>
2025-10-16 14:58:15 +0200

    prepaired tester for norms

--- tester.c
+++ tester.c
@@ -1,61 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omalovic <omalovic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 14:28:01 by omalovic          #+#    #+#             */
+/*   Updated: 2025/10/16 14:57:24 by omalovic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int	run_cub3d_on_file(const char *path, const char *filename, int file_count)
 {
-    const char *path_bad = "./maps/bad/";
-    const char *path_good = "./maps/good/";
-    DIR *dir_bad = opendir(path_bad);
-    DIR *dir_good = opendir(path_good);
+	char	buffer[4096];
 
-    if (!dir_bad || !path_good)
-    {
-        perror("opendir");
-        return 1;
-    }
-    struct dirent *entry;
-    char buffer[4096];
-    
-    int file_count = 1;
-    printf("\n==========BAD MAPS==========\n");
-    while ((entry = readdir(dir_bad)) != NULL)
-    {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-            continue;
-        printf("\n----------FILE %d----------\n", file_count);
-        snprintf(buffer, sizeof(buffer) - 1, "./cub3d %s%s", path_bad, entry->d_name);
-        printf("path: %s\n", buffer);
-        int code = system(buffer);
-        if (code == -1)
-        {
-            perror("system");
-            break;
-        }
-        file_count++;
-        sleep(1);
-    }
-    printf("\n\n==========GOOD MAPS==========\n");
-    // while ((entry = readdir(dir_good)) != NULL)
-    // {
-    //     if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-    //         continue;
-    //     printf("\n----------FILE %d----------\n", file_count);
-    //     snprintf(buffer, sizeof(buffer) - 1, "./cub3d %s%s", path_bad, entry->d_name);
-    //     printf("path: %s\n", buffer);
-    //     int code = system(buffer);
-    //     if (code == -1)
-    //     {
-    //         perror("system");
-    //         break;
-    //     }
-    //     file_count++;
-    //     sleep(1);
-    // }
-    closedir(dir_bad);
-    closedir(dir_good);
+	snprintf(buffer, sizeof(buffer) - 1, "./cub3d %s%s", path, filename);
+	printf("\n----------FILE %d----------\n", file_count);
+	printf("path: %s\n", buffer);
+	if (system(buffer) == -1)
+	{
+		perror("system");
+		return (-1);
+	}
+	return (0);
+}
+
+void	process_dir(const char *path)
+{
+	DIR				*dir;
+	struct dirent	*entry;
+	int				file_count;
+
+	file_count = 1;
+	dir = opendir(path);
+	if (!dir)
+		return (perror("opendir"));
+	printf("\n==========BAD MAPS==========\n");
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+		{
+			entry = readdir(dir);
+			continue ;
+		}
+		if (run_cub3d_on_file(path, entry->d_name, file_count) == -1)
+			break ;
+		file_count++;
+		sleep(1);
+		entry = readdir(dir);
+	}
+	closedir(dir);
+}
+
+int	main(void)
+{
+	char	*path_bad;
+
+	path_bad = strdup("./maps/bad/");
+	if (!path_bad)
+		return (1);
+	process_dir(path_bad);
+	free(path_bad);
+	return (0);
 }
 
 /* 
